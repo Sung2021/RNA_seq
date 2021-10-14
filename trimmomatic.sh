b530c8de334479ce7763a12f11be59d13082b5a7
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2021-10-14 16:04:00 -0400

    Create trimmomatic.sh

---
+++ trimmomatic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+path=~/Desktop/Sung_work/fastq/19092_50_01
+
+cd $path/
+mkdir trimmed
+for input in ko1 ko2 ko3 wt1 wt2 wt3 ; do java -jar ~/Desktop/software/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 10 \
+$path/${input}.R1.fastq.gz $path/${input}.R2.fastq.gz \
+$path/trimmed/${input}.R1.paired.fastq.gz $path/trimmed/${input}.R1.unpaired.fastq.gz \
+$path/trimmed/${input}.R2.paired.fastq.gz $path/trimmed/${input}.R2.unpaired.fastq.gz \
+CROP:110; done
