1f8b4357cb37566b9320f5e1020c622b9b6d6d02
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-04-15 10:26:00 -0400

    Create zscore.r

---
+++ R/format/zscore.r
@@ -0,0 +1,11 @@
+## zscore function 
+zscore <- function(input.data = input.data){
+  input.data.rowsums <- rowSums(input.data)
+  input.data.mean <- rowMeans(input.data)
+  input.data.sd <- matrixStats::rowSds(as.matrix(input.data))
+  names(input.data.sd) <- rownames(input.data)
+  zscore <- (input.data-input.data.mean)/input.data.sd
+  return(zscore)
+}
+
+zscore_result <- zscore(input.data  = input.data)
