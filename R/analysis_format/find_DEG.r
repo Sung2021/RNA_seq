7ab31a006204dc7988a4eb43636e5ad5a68adead
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-04-18 15:00:27 -0400

    Create find_DEG.r

---
+++ R/analysis_format/find_DEG.r
@@ -0,0 +1,29 @@
+######   #################
+input1 <- 'Tcm_irr'
+input2 <- 'Tem_irr'
+
+count.input <- count.mtx[,c(grep(input1, colnames(count.mtx)),
+                            grep(input2, colnames(count.mtx)))]  ## input for count 
+info <- data.frame(matrix(nrow = ncol(count.input), ncol = 2))
+colnames(info) <- c('sample', 'condition')
+info$sample <- colnames(count.input)
+info$condition <- substr(info$sample, 1,7)
+info$condition <- factor(info$condition, levels = c(input1,input2))
+info 
+dds <- DESeqDataSetFromMatrix(count.input, info, ~ condition)
+dim(dds)
+dds <- DESeq(dds)
+res <- results(dds)
+dim(res)
+res <- data.frame(res)
+res %>% write.csv('RNA_seq/Iowa_Tcm Tem irradiation/dds.res.7246.genes.tcm_irr.tem_irr.csv')
+
+
+is.na(res$padj) %>% table()
+res.filtered <- res[!(is.na(res$padj)),]
+dim(res.filtered)
+res.filtered <- res.filtered %>% 
+  filter(log2FoldChange >= log2(2)|log2FoldChange <= -log2(2)) %>% 
+  filter(padj < 0.05)
+res.filtered %>% dim()
+res.filtered %>% rownames()
