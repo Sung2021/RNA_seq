07315839844ec5fea3b484bc4ac79f8a5e01ec66
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-05-06 12:06:34 -0400

    Update volcano.r

--- R/format/volcano.r
+++ R/format/volcano.r
@@ -1,3 +1,27 @@
+## basic plot
+## input mtx: res
+res <- read.csv('res_all/Tcm.ctr_irr.626.genes.2022.04.22.csv',
+                row.names = 1)
+res$gene <- ''
+res %>% ggplot(aes(log2FoldChange, -log10(padj), 
+                                 label=gene)) +
+  geom_point(size=1, alpha=0.5) + theme_classic() +
+  geom_vline(xintercept = c(-1,1), color='red') +
+  geom_hline(yintercept = -log10(0.05), color='blue') +
+  geom_text(hjust = -0.05, nudge_x = 0.05)
+
+
+
+
+
+
+
+
+
+
+
+
+
 res.threshold.0.5 %>% ggplot(aes(log2FoldChange, -log10(padj))) +
   geom_point(size=1, alpha=0.5) + theme_classic() +
   geom_vline(xintercept = c(-1,1), color='red') +
