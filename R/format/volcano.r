3062be3cd9cc6bb98bee707f150c55388d4eb1a3
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-05-09 11:37:54 -0400

    Update volcano.r

--- R/format/volcano.r
+++ R/format/volcano.r
@@ -15,13 +15,6 @@ res %>% ggplot(aes(log2FoldChange, -log10(padj),
 
 
 
-
-
-
-
-
-
-
 res.threshold.0.5 %>% ggplot(aes(log2FoldChange, -log10(padj))) +
   geom_point(size=1, alpha=0.5) + theme_classic() +
   geom_vline(xintercept = c(-1,1), color='red') +
@@ -87,4 +80,9 @@ res.df %>% ggplot(aes(log2FoldChange, -log10(padj),
   geom_vline(xintercept = c(-1,1), color='red') +
   geom_hline(yintercept = -log10(0.05), color='blue') +
   geom_text(hjust = -0.05, nudge_x = 0.05)
+
+
+
+cowplot::plot_grid(p1,p3,p2, ncol = 3)
+
 
